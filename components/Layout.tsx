560dd483f6145ca11696cf5158150a8e9cc6a1f2
minamAn <minamhunmin@gmail.com>
2022-06-21 13:10:28 +0900

    update items id layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { cls } from "../pages/libes/utils";
 
 interface LayoutProps {
@@ -13,10 +14,37 @@ export default function Layout({
   children,
   hasTabBar,
 }: LayoutProps) {
+  const router = useRouter();
+  const onClick = () => {
+    router.back();
+  };
   return (
     <div>
-      <div className="bg-white w-full max-w-xl text-lg font-medium py-3 fixed text-gray-700 border-b top-0 flex items-center justify-center">
+      <div
+        className={cls(
+          !canGoBack ? "justify-center" : "",
+          "bg-white w-full max-w-xl text-lg font-medium py-3 fixed text-gray-700 border-b top-0 flex items-center"
+        )}
+      >
         {title ? <span>{title}</span> : null}
+        {canGoBack ? (
+          <button onClick={onClick}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15 19l-7-7 7-7"
+              />
+            </svg>
+          </button>
+        ) : null}
       </div>
       <div className={cls("pt-12", hasTabBar ? "pb-24" : "")}>{children}</div>
       {hasTabBar ? (
